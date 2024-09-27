9eb31f6b055e442697697f7977429d38c7b8863a
Amine-youcode <bourhymamine@gmail.com>
2024-09-27 11:59:56 +0200

    table2

---
+++ table2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int T[11];
+    int b;
+    
+    printf("entre nombre la taile \n");
+    
+    
+    for (a=0;a<11;a++){
+        T[11]=0;
+        printf("T[%i]= ",a);
+        scanf("%i",&T[a]);
+        b=0;
+        b=b+T[a];
+        
+    }
